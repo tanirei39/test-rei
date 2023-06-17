a1123d0af12ad38389a71bd95c43c2a6db125c89
ReinaTaniguchi <134829647+tanirei39@users.noreply.github.com>
2023-06-17 23:26:00 +0900

    feat: message list

--- main.go
+++ main.go
@@ -3,36 +3,116 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// マスタからSELECTしたデータをマッピングする構造体
+type MESSAGE struct {
+	Id   string `db:"message_id"`
+	Text string `db:"message_text"`
+}
+
 func main() {
 	// http.Handle("/foo", fooHandler)
 	fmt.Println("run server")
-
-	// http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// })
 
 	// http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
 	// 	fmt.Fprintf(w, "れいなだお, %q", html.EscapeString(r.URL.Path))
 	// })
 
 	connectMySQL()
-	sqlInsert()
+	// "user-form"へのリクエストを関数で処理する
+	http.HandleFunc("/message-form", HandlerUserForm)
+	// "message-confirm"へのリクエストを関数で処理する
+	http.HandleFunc("/message-confirm", RedirectHandlerMessageConfirm)
+	// "user-confirm"へのリクエストを関数で処理する
+	http.HandleFunc("/message-list", HandlerUserConfirm)
+
+	// sqlInsert()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
-func sqlInsert() {
+// 投稿入力画面
+func HandlerUserForm(w http.ResponseWriter, r *http.Request) {
+	// テンプレートをパースする
+	tpl := template.Must(template.ParseFiles("templates/message-form.gtpl"))
+
+	// テンプレートに出力する値をマップにセット
+	values := map[string]string{}
+
+	// マップを展開してテンプレートを出力する
+	if err := tpl.ExecuteTemplate(w, "message-form.gtpl", values); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func RedirectHandlerMessageConfirm(w http.ResponseWriter, r *http.Request) {
+	//データ登録
+	messageInsert(r.FormValue("message_text"))
+	http.Redirect(w, r, "/message-list", 301)
+}
+
+// 投稿一覧画面
+func HandlerUserConfirm(w http.ResponseWriter, r *http.Request) {
+	// テンプレートをパースする
+	tpl := template.Must(template.ParseFiles("templates/message-list.gtpl"))
+
+	// データベース接続
+	db, err := sql.Open("mysql", "root:rei0309@tcp(localhost:3306)/testrei")
+	if err != nil {
+		// log.Fatal(err)
+		fmt.Println("データベース接続に失敗しました。")
+	}
+	// deferで処理終了前に必ず接続をクローズする
+	defer db.Close()
+
+	// プリペアードステートメント
+	ins, err := db.Prepare("SELECT message_id,message_text FROM messages")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// クエリ実行
+	rows, err := ins.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var ml []MESSAGE
+
+	for rows.Next() {
+		// 構造体宣言
+		var message MESSAGE
+		err = rows.Scan(&message.Id, &message.Text)
+		// 配列にScan結果を追加
+		ml = append(ml, message)
+	}
+
+	if err != nil {
+		fmt.Println("データベース接続失敗")
+		log.Fatal(err)
+	} else {
+		fmt.Println("データベース接続成功")
+	}
+
+	// マップを展開してテンプレートを出力する
+	if err := tpl.ExecuteTemplate(w, "message-list.gtpl", ml); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func messageInsert(message string) {
 	// データベースのハンドルを取得する
 	db, err := sql.Open("mysql", "root:rei0309@tcp(localhost:3306)/testrei")
 	if err != nil {
 		// ここではエラーを返さない
-		log.Fatal(err)
+		fmt.Println("データベース接続に失敗しました。")
 	}
 	defer db.Close()
 
@@ -44,7 +124,7 @@ func sqlInsert() {
 	defer ins.Close()
 
 	// SQLの実行
-	res, err := ins.Exec("こんばんは")
+	res, err := ins.Exec(message)
 	if err != nil {
 		log.Fatal(err)
 	}
